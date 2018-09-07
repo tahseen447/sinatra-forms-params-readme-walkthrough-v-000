5d6fb519c693eb7eca547954f4f35cac6f13176f
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-07 16:06:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -8,4 +8,9 @@ class App < Sinatra::Base
 
   # Add your post route and action below
 
+  post '/food' do
+    name = params[:name]
+    fav_food = params[:food]
+    "My name is #{name}, and I love #{fav_food}."
+  end
 end
